820caa5c1d94a4aacf8a3068b5a2a4a617f88987
Kabilan <kabilananbukani@gmail.com>
2022-02-26 15:19:04 +0800

    hight assest hidden

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -28,7 +28,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
-
+111
         private Camera m_Camera;
         private bool m_Jump;
         private float m_YRotation;
